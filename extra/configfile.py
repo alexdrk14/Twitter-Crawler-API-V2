cbfc5565706943698678b121c70a2ec5ab36f62b
Alexander Shevtsov <alex.drk14@gmail.com>
2022-11-11 19:04:57 +0200

    Configuration file update

--- extra/configfile.py
+++ extra/configfile.py
@@ -2,13 +2,13 @@
 
 """Configuration of MongoDB connector, used for storing the data in DB and filtering already seen users and tweets"""
 DBCONFIG={
-          "address"   : '127.0.0.1',   """IP address of MongoDB, like '127.0.0.1' in string type"""
-          "port"      : 27017,  """Port of MongoDB, like 27017 in integer type"""
-          "db"        : 'Enter_Your_Mongo_DB_Name',   """Name of MongoDB Database, like 'CollectedDatabase' in string type"""
+          "address"   : '127.0.0.1',   #IP address of MongoDB, like '127.0.0.1' in string type"""
+          "port"      : 27017, #Port of MongoDB, like 27017 in integer type"""
+          "db"        : 'Your_Mongo_DB_Name', #Name of MongoDB Database, like 'CollectedDatabase' in string type"""
           }
 
 """Bearer Token provided by Twitter Developer API"""
-bearer_token = ''
+bearer_token = 'Your_bearer_token'
 
 """Filename + path in case if file located in different path. This file contain dictionary of filtering keywords and tags. See example in keywords.txt.
 For more complicated rules please check the Twitter Developer V2 manual"""
